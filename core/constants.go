e6cea2d33f627650f7f550eb67365ce052a7d578
Matt Solomon <matt@mattsolomon.dev>
2026-08-21 10:43:35 -0700

    docs: source EtherFi contract labels

--- core/constants.go
+++ core/constants.go
@@ -53,11 +53,13 @@ const (
 	OPL1StandardBridge       = "0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1"
 	OPL2StandardBridge       = "0x4200000000000000000000000000000000000010"
 	SaferSafes               = "0xA8447329e52F64AED2bFc9E7a2506F7D369f483a"
-	EtherFiSpokeProxy        = "0xdffcC3536D932eb51Df51a7F5FA407c4270d5308"
-	EtherFiSpokeImpl         = "0xA1f75D801633a1941cae6670352d627884dC3b68"
-	SafeMigration141         = "0x526643F69b81B008F46d95CD5ced5eC0edFFDaC6"
-	SafeMasterCopy141        = "0x41675c099f32341bf84bfc5382af534df5c7461a"
-	SafeFallbackHandler141   = "0xfd0732dc9e303f09fcef3a7388ad10a83459ec99"
+	// EtherFiSpokeProxy is an EIP-1967 proxy whose implementation is EtherFiSpokeImpl.
+	// Verified implementation source: https://explorer.optimism.io/address/0xA1f75D801633a1941cae6670352d627884dC3b68?tab=contract
+	EtherFiSpokeProxy      = "0xdffcC3536D932eb51Df51a7F5FA407c4270d5308"
+	EtherFiSpokeImpl       = "0xA1f75D801633a1941cae6670352d627884dC3b68"
+	SafeMigration141       = "0x526643F69b81B008F46d95CD5ced5eC0edFFDaC6"
+	SafeMasterCopy141      = "0x41675c099f32341bf84bfc5382af534df5c7461a"
+	SafeFallbackHandler141 = "0xfd0732dc9e303f09fcef3a7388ad10a83459ec99"
 )
 
 // Functions on ERC20 tokens that require decimal adjustment
